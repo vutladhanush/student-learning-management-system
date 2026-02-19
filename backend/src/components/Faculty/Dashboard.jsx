5604f79388181bbc709a5d10fbdf8bdf54da2f31
Dhanush1231 <vutla.dhanush08@gmail.com>
2026-02-19 21:54:14 +0530

    Remove all files

--- backend/src/components/Faculty/Dashboard.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const FacultyDashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
--- backend/src/components/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home = () => {
-  return <h1>Welcome Home</h1>;
-};
-
-export default Home;
